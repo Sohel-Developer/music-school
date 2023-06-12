2bf10f3588682e5c1fad53d158af74520011aeee
Sohel-Developer <sohel.webdevoloper@gmail.com>
2023-06-12 16:15:31 +0600

    Users Saved in post method

--- index.js
+++ index.js
@@ -43,17 +43,17 @@ async function run() {
         app.post('/users', async (req, res) => {
 
             const user = req.body;
-            console.log(user);
-            // const query = { email: user.email }
-            // const existingUser = await usersCollection.findOne(query);
+            const query = { email: user?.email }
+            const existingUser = await usersCollection.findOne(query);
 
-            // if (existingUser) {
-            //     return res.send({ message: 'user already exists' })
-            // }
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
 
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+
 
 
 

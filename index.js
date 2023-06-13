65a5a1eaf8c1607e67b7146472e386264684048f
Sohel-Developer <sohel.webdevoloper.com>
2023-06-13 17:46:30 +0600

    Update Link

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ async function run() {
             res.send(result);
 
         })
-        app.patch('/users/instructor/:id', async (req, res) => {
+        app.patch('/instructor/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
